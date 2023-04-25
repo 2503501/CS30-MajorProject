b1708a8c997a34357293bdf104b02f3114875eaa
2503501 <126823117+2503501@users.noreply.github.com>
2023-04-25 15:20:25 -0600

    created some new background and started the grid array

--- sketch.js
+++ sketch.js
@@ -11,6 +11,16 @@ let tileSize;
 let gamestate = "main";
 let mainMenuBackground;
 
+let pieceSelected = false;
+
+let grid = [
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0]
+];
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   mainMenuBackground = loadImage("mainmenu.jpg");
@@ -23,14 +33,20 @@ function draw() {
 }
 
 function mousePressed(){
-  updateBackgroundStatus()
+  updateBackgroundStatus();
+  console.log(mouseX);
+  console.log(mouseY);
+  if (!pieceSelected){
+    let x = Math.floor(mouseX/tileSize - (width/2 - tileSize *4 )/tileSize);
+    let y = Math.floor(mouseY/tileSize - (height/2 - tileSize *4 )/tileSize);
+  }
 }
 
 function drawBackground(){
   if (gamestate === "main"){
     background(mainMenuBackground);
   }
-  else{
+  if (gamestate === "adventure"){
     for (let x = 0; x < 10; x++){
       for (let y = 0; y < 5; y++){
         if ((x+y+1 ) % 2){
@@ -42,12 +58,14 @@ function drawBackground(){
         rect(x*tileSize + backgroundOffset, y*tileSize + height/6, tileSize, tileSize);
       }
     }
+    fill(218, 160, 109);
+    rect(backgroundOffset, 0, tileSize*6, tileSize);
   }
 }
 
 
 function updateBackgroundStatus(){
   if (mouseX >988 && mouseX < 1740 && mouseY >146 && mouseY < 300 && gamestate === "main"){
-    gamestate = "adventure"
+    gamestate = "adventure";
   }
 }
