f65b6829622986569cd4267d9ff78df643cef08c
2503501 <126823117+2503501@users.noreply.github.com>
2023-04-24 23:09:25 -0600

    started main menu backgroun

--- sketch.js
+++ sketch.js
@@ -9,29 +9,45 @@
 let backgroundOffset = 100;
 let tileSize;
 let gamestate = "main";
-let mainMenuground = loadImage("blackbishop.png");
+let mainMenuBackground;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  mainMenuBackground = loadImage("mainmenu.jpg");
   tileSize = height/6;
 }
 
 function draw() {
   background(200);
   drawBackground();
+}
 
+function mousePressed(){
+  updateBackgroundStatus()
 }
 
 function drawBackground(){
-  for (let x = 0; x < 10; x++){
-    for (let y = 0; y < 5; y++){
-      if ((x+y+1 ) % 2){
-        fill(28,133,30);
+  if (gamestate === "main"){
+    background(mainMenuBackground);
+  }
+  else{
+    for (let x = 0; x < 10; x++){
+      for (let y = 0; y < 5; y++){
+        if ((x+y+1 ) % 2){
+          fill(28,133,30);
+        }
+        else{
+          fill(133,186,68);
+        }
+        rect(x*tileSize + backgroundOffset, y*tileSize + height/6, tileSize, tileSize);
       }
-      else{
-        fill(133,186,68);
-      }
-      rect(x*tileSize + backgroundOffset, y*tileSize + height/6, tileSize, tileSize);
     }
+  }
+}
+
+
+function updateBackgroundStatus(){
+  if (mouseX >988 && mouseX < 1740 && mouseY >146 && mouseY < 300 && gamestate === "main"){
+    gamestate = "adventure"
   }
 }
